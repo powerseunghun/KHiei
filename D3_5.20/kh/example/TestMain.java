5f97725285ae735ca5a4e65328a8234a492e8f5a
powerseunghun <size411@naver.com>
2020-05-20 15:02:50 +0900

    day3 real last

---
+++ D3_5.20/kh/example/TestMain.java
@@ -0,0 +1,10 @@
+package com.kh.example;
+
+public class TestMain {
+	public static void main(String[] args) {
+		Example a = new Example();
+//		a.example1();
+//		a.example2();
+		a.example3();
+	}
+}
