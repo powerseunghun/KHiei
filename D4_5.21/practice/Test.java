f02c181823a353f04a39f7e5bb5f09f575516aa5
powerseunghun <size411@naver.com>
2020-05-22 09:25:34 +0900

    day4 practice

---
+++ D4_5.21/practice/Test.java
@@ -0,0 +1,12 @@
+package com.kh.practice;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Example e = new Example();
+		/*e.sample1();
+		e.sample2();
+		e.sample3();
+		e.sample4();*/
+	}
+}
