688c268ee207a6d52ed0fcfd322740f67a15e46d
powerseunghun <seunghun457@gmail.com>
2020-05-31 14:43:12 +0900

    add homework soruce 1/2

---
+++ D10_5.29/homework_1_source/LogicHomework/src/com/kh/home/main/TestMain.java
@@ -0,0 +1,12 @@
+package com.kh.home.main;
+
+import com.kh.home.view.Menu;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		Menu m = new Menu();
+		m.displayMenu();
+	}
+
+}
