dc98cbfdc02974fe49add847309d27f93217a457
powerseunghun <size411@naver.com>
2020-07-16 10:21:03 +0900

    day44 start

---
+++ 1. JAVA/D3_5.20/kh/example2/TestCasting.java
@@ -0,0 +1,9 @@
+package com.kh.example2;
+
+public class TestCasting {
+	public static void main(String[] args) {
+		CastingSample a = new CastingSample();
+//		a.printUniCode();
+		a.calculatorScore();
+	}
+}
--- D3_5.20/kh/example2/TestCasting.java
+++
@@ -1,9 +0,0 @@
-package com.kh.example2;
-
-public class TestCasting {
-	public static void main(String[] args) {
-		CastingSample a = new CastingSample();
-//		a.printUniCode();
-		a.calculatorScore();
-	}
-}
